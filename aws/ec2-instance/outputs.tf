b2a81bd2bdbed2749065fa3b47a4ec7f51438b8c
Abhishek-Sontakke <sontakkeabhishek27@gmail.com>
2024-08-26 22:25:59 +0530

    Update outputs.tf

--- aws/ec2-instance/outputs.tf
+++ aws/ec2-instance/outputs.tf
@@ -1,15 +1,15 @@
 output "instance_id" {
   description = "The ID of the EC2 instance"
-  value       = aws_instance.my_instance.id
+  value       = aws_instance.app_server.id
 }
 
 output "instance_private_ip" {
   description = "The private IP address of the EC2 instance"
-  value       = aws_instance.my_instance.private_ip
+  value       = aws_instance.app_server.private_ip
 }
 
 output "instnace_public_ip" {
   description = "The public IP address of the EC2 instance"
-  value       = aws_instance.my_instance.public_ip
+  value       = aws_instance.app_server.public_ip
 }
 
